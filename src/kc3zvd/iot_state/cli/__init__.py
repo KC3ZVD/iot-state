19d93cfa40fadac998af8e67b473da7dcd1c8b2d
KC3ZVD <github@kc3zvd.net>
2024-12-20 23:27:24 -0500

    feat(cli): convert to auto env var prefix for options values

--- src/kc3zvd/iot_state/cli/__init__.py
+++ src/kc3zvd/iot_state/cli/__init__.py
@@ -3,26 +3,28 @@
 # SPDX-License-Identifier: MIT
 import click
 from kc3zvd.iot_state.__about__ import __version__
+from kc3zvd.iot_state.publishers import mqtt
 
-
-@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=False)
+@click.group(context_settings={"help_option_names": ["-h", "--help"], "auto_envvar_prefix": "IOT"}, invoke_without_command=False)
 @click.version_option(version=__version__, prog_name="iot-state")
-@click.option('--redis-host', help="Redis instance host", envvar='IOT_REDIS_HOST', required=True)
-@click.option('--redis-port', help="Redis instance port", envvar='IOT_REDIS_PORT', required=True, type=int)
-@click.option('--redis-db', help="Redis instance DB number", envvar='IOT_REDIS_DB', required=True, type=int)
+@click.option('--redis-host', help="Redis instance host", required=True)
+@click.option('--redis-port', help="Redis instance port", required=True, type=int)
+@click.option('--redis-db', help="Redis instance DB number", required=True, type=int)
 @click.pass_context
 def iot_state(ctx, redis_host, redis_port, redis_db):
   ctx.ensure_object(dict)
-  ctx.obj['REDIS_HOST'] = redis_host
-  ctx.obj['REDIS_PORT'] = redis_port
-  ctx.obj['REDIS_DB'] = redis_db
+  ctx.obj['redis_host'] = redis_host
+  ctx.obj['redis_port'] = redis_port
+  ctx.obj['redis_db'] = redis_db
 
 @iot_state.command()
-@click.option('--platform', help="The platform to publish to", envvar='IOT_PUBLISHER_PLATFORM', required=True, 
+@click.option('--platform', help="The platform to publish to", required=True, 
               type=click.Choice(['mqtt'], case_sensitive=False))
 @click.pass_context
 def publisher(ctx, platform):
-  click.echo(f"Host: {ctx.obj['redis_host']} \nPort: {ctx.obj['redis_port']}\nDB: {ctx.obj['redis_db']}")
   match platform:
     case 'mqtt':
+      mqtt.subscribe(redis_host=ctx.obj['redis_host'], redis_port=ctx.obj['redis_port'], redis_db=ctx.obj['redis_db'])
       click.echo("mqtt platform selected")
+    case _:
+      die()
--- src/kc3zvd/iot_state/publishers/mqtt.py
+++ src/kc3zvd/iot_state/publishers/mqtt.py
@@ -1,10 +1,10 @@
-import redis
 from __future__ import annotations
+import redis
 # prefix/device_type/area_name/device_name/state_class
 
 def subscribe(redis_host: str, redis_port: int, redis_db: int):
   r = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db,  decode_responses=True)
-  p = r.pubsub(ignore_subscribed_messages=True)
+  p = r.pubsub()
   p.subscribe("iot-state-publish")
   for message in p.listen():
     if message["type"] == "message":
