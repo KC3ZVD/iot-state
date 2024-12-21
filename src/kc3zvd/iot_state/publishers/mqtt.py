bffa7684343bac4c49c4ba1c85cc4821c68db782
KC3ZVD <github@kc3zvd.net>
2024-12-20 22:28:48 -0500

    feat(publishers.mqtt): initial pub/sub listener

---
+++ src/kc3zvd/iot_state/publishers/mqtt.py
@@ -0,0 +1,11 @@
+import redis
+from __future__ import annotations
+# prefix/device_type/area_name/device_name/state_class
+
+def subscribe(redis_host: str, redis_port: int, redis_db: int):
+  r = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db,  decode_responses=True)
+  p = r.pubsub(ignore_subscribed_messages=True)
+  p.subscribe("iot-state-publish")
+  for message in p.listen():
+    if message["type"] == "message":
+      print(f"Received: {message['data'].decode('utf-8')}")
